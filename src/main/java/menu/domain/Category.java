34ae0512dd16beb030a865679a2a1c3f8ff4c51d
MODUGGAGI <kiroro0814@naver.com>
2026-01-06 21:11:59 +0900

    feat(domain): 카테고리 추첨을 위한 번호 추가
    카테고리 추첨을 위한 번호를 추가했습니다.

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,15 +1,31 @@
 package menu.domain;
 
+import java.util.Arrays;
+import menu.message.ErrorMessage;
+
 public enum Category {
-    JAPANESE("일식"),
-    KOREAN("한식"),
-    CHINESE("중식"),
-    ASIAN("아시안"),
-    WESTERN("양식");
+    JAPANESE("일식", 1),
+    KOREAN("한식", 2),
+    CHINESE("중식", 3),
+    ASIAN("아시안", 4),
+    WESTERN("양식", 5),;
 
     private final String name;
+    private final int number;
 
-    Category(String name) {
+    Category(String name, int number) {
         this.name = name;
+        this.number = number;
+    }
+
+    public static Category get(int categoryNumber) {
+        return Arrays.stream(values())
+                .filter(category -> category.number == categoryNumber)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.CATEGORY_NOT_FOUND.getMessage()));
+    }
+
+    public String getName() {
+        return name;
     }
 }
