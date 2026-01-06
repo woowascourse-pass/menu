9bb048a63dc2510a71e4d4ea0b45fd937902ba12
MODUGGAGI <kiroro0814@naver.com>
2026-01-06 19:55:23 +0900

    feat(domain): 카테고리, 음식 추가
    카테고리와 음식은 고정되어있기 때문에 ENUM으로 추가하였습니다.

---
+++ src/main/java/menu/domain/Category.java
@@ -0,0 +1,15 @@
+package menu.domain;
+
+public enum Category {
+    JAPANESE("일식"),
+    KOREAN("한식"),
+    CHINESE("중식"),
+    ASIAN("아시안"),
+    WESTERN("양식");
+
+    private final String name;
+
+    Category(String name) {
+        this.name = name;
+    }
+}
