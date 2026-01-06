939b2ec43216366dda47c4712e18d8fa3fceddd7
MODUGGAGI <kiroro0814@naver.com>
2026-01-06 19:10:29 +0900

    feat(view): 출력 뷰 생성
    출력용 뷰를 생성해 에러를 출력하게 했습니다.

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,9 @@
+package menu.view;
+
+public class OutputView {
+
+    public void printError(String errorMessage) {
+        System.out.println(errorMessage);
+        System.out.println();
+    }
+}
