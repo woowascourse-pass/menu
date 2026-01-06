0bafd967518a46b5c84ee9fd43a0172f2e6193ce
MODUGGAGI <kiroro0814@naver.com>
2026-01-06 21:19:42 +0900

    feat(domain): 음식 추첨 기능 구현
    사용자가 못 먹는 음식을 제외하고, 일주일에 같은 카테고리는 2번까지만 선택 가능하게 하고, 각 코치들은 각각 자기가 먹을 음식들이 일주일 동안 중복되지 않게 구현했습니다.

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -2,26 +2,26 @@ package menu.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import menu.dto.RecommendMenuResult;
 import menu.message.ErrorMessage;
 
 public class Coach {
 
     private final String name;
     private final List<Food> hateFoods = new ArrayList<>();
+    private final List<Food> alreadyEat = new ArrayList<>();
 
     public Coach(String name) {
         validateName(name);
         this.name = name;
     }
 
-    public boolean equalsName(String name) {
-        return this.name.equals(name);
+    public void clearHateFood() {
+        hateFoods.clear();
     }
 
-    private void validateName(String name) {
-        if (name.length() < 2 || 4 < name.length()) {
-            throw new IllegalArgumentException(ErrorMessage.COACH_NAME_OUT_OF_RANGE.getMessage());
-        }
+    public boolean equalsName(String name) {
+        return this.name.equals(name);
     }
 
     public void addHateFood(List<String> hateFoodNames) {
@@ -38,13 +38,40 @@ public class Coach {
         }
     }
 
+    public void eatFood(Food food) {
+        alreadyEat.add(food);
+    }
+
+    private void validateName(String name) {
+        if (name.length() < 2 || 4 < name.length()) {
+            throw new IllegalArgumentException(ErrorMessage.COACH_NAME_OUT_OF_RANGE.getMessage());
+        }
+    }
+
     private void validateHateFood(List<String> hateFoodNames) {
         if (hateFoodNames.size() > 3) {
             throw new IllegalArgumentException(ErrorMessage.HATE_FOOD_OUT_OF_RANGE.getMessage());
         }
     }
 
-    public void clearHateFood() {
-        hateFoods.clear();
+    public boolean checkFoodUnavailable(String foodName) {
+        Food food = Food.searchFood(foodName);
+
+        if (hateFoods.contains(food)) {
+            return true;
+        }
+
+        if (alreadyEat.contains(food)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public RecommendMenuResult makeResult() {
+        List<String> foodNames = alreadyEat.stream().map(Food::getName)
+                .toList();
+
+        return new RecommendMenuResult(name, foodNames);
     }
 }
--- src/main/java/menu/domain/Coaches.java
+++ src/main/java/menu/domain/Coaches.java
@@ -1,11 +1,15 @@
 package menu.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import menu.dto.RecommendMenuResult;
+import menu.dto.ResultHeader;
 import menu.message.ErrorMessage;
 
 public class Coaches {
     private final List<Coach> coaches = new ArrayList<>();
+    private final List<Category> weekCategories = new ArrayList<>();
 
     public List<String> createCoaches(List<String> coachNames) {
         validateCoachesNumber(coachNames);
@@ -32,5 +36,68 @@ public class Coaches {
         return coaches.stream().filter(coach -> coach.equalsName(coachName))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.COACH_NOT_FOUND.getMessage()));
+    }
+
+    // 무작위 뽑기
+    public ResultHeader startLottery() {
+
+        for (int day = 0; day < 5; day++) {
+            Category category = randomCategory();
+            weekCategories.add(category);
+            recommendRandomMenu(category);
+        }
+
+        return getResultHeader();
+    }
+
+    private ResultHeader getResultHeader() {
+        List<RecommendMenuResult> menuResults = new ArrayList<>();
+
+        for (Coach coach : coaches) {
+            menuResults.add(coach.makeResult());
+        }
+
+        List<String> categoryResults = weekCategories.stream().map(Category::getName).toList();
+
+        return new ResultHeader(categoryResults, menuResults);
+    }
+
+    private void recommendRandomMenu(Category category) {
+        for (Coach coach : coaches) {
+            Food food = randomFood(coach, category);
+            coach.eatFood(food);
+        }
+    }
+
+    private Category randomCategory() {
+        while (true) {
+            Category category = Category.get(Randoms.pickNumberInRange(1, 5));
+            if (checkAvailable(category)) {
+                return category;
+            }
+        }
+    }
+
+    // 이용 불가하면 false 반환
+    private boolean checkAvailable(Category category) {
+        // 이용 가능한지 봐야됨.
+        int count = (int) weekCategories.stream()
+                .filter(eachCategory -> eachCategory.equals(category))
+                .count();
+
+        return count < 2;
+    }
+
+    private Food randomFood(Coach coach, Category category) {
+        while (true) {
+            List<String> menu = Food.getFoodByCategory(category);
+            String foodName = Randoms.shuffle(menu).get(0);
+
+            if (coach.checkFoodUnavailable(foodName)) {
+                continue;
+            }
+
+            return Food.searchFood(foodName);
+        }
     }
 }
