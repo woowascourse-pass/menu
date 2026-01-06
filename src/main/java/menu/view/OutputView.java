78d43026c337dc0a69c125b50964ab685a98fe2f
MODUGGAGI <kiroro0814@naver.com>
2026-01-06 21:23:45 +0900

    feat(view): 음식 추첨 기능 구현
    Controller로 부터 전달받은 결과를 화면에 보여주기 위한 메소드를 구현했습니다.

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,9 +1,44 @@
 package menu.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import menu.dto.RecommendMenuResult;
+import menu.dto.ResultHeader;
+
 public class OutputView {
+
+    private static final String LOTTERY_RESULT = "메뉴 추천 결과입니다.";
+    private static final String WEEK_DAYS = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]";
+    private static final String CATEGORY = "카테고리";
+    private static final String COMPLETE_LOTTERY = "추천을 완료했습니다.";
 
     public void printError(String errorMessage) {
         System.out.println(errorMessage);
         System.out.println();
+    }
+
+    public void printResult(ResultHeader result) {
+        System.out.println();
+        System.out.println(LOTTERY_RESULT);
+        System.out.println(WEEK_DAYS);
+
+        System.out.println(printEachLine(CATEGORY, result.categoryName()));
+
+        printEachCoachMenu(result.eachOne());
+
+        System.out.println();
+        System.out.println(COMPLETE_LOTTERY);
+    }
+
+    private void printEachCoachMenu(List<RecommendMenuResult> menuResults) {
+        for (RecommendMenuResult menuResult : menuResults) {
+            System.out.println(printEachLine(menuResult.coachName(), menuResult.foodNames()));
+        }
+    }
+
+    private String printEachLine(String beginning, List<String> list) {
+        return "[ " + beginning + " | " + list.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(" | ", "", " ]"));
     }
 }
