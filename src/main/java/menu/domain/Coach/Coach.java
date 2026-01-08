20a9deb7b629e8e29f87a113a0b74fda4e4b8df6
Junha Park <poiu7825@pusan.ac.kr>
2026-01-08 18:14:02 +0900

    feat: Coach 클래스 기본 로직 추가

--- src/main/java/menu/domain/Coach/Coach.java
+++ src/main/java/menu/domain/Coach/Coach.java
@@ -2,12 +2,12 @@ package menu.domain.Coach;
 
 import java.util.List;
 
-public class Coach {
-    private final String name;
-    private final List<String> allergyMenuNames;
+public record Coach(
+        String name,
+        List<String> allergyMenuNames
+) {
 
-    public Coach(String name, List<String> allergyMenuNames) {
-        this.name = name;
-        this.allergyMenuNames = allergyMenuNames;
+    public boolean isEat(String menuName) {
+        return !allergyMenuNames.contains(menuName);
     }
 }
