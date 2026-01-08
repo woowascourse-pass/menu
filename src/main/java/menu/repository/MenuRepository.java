d1d0b0275e5a40946e7a9668ecc89d24a13aaef3
Junha Park <poiu7825@pusan.ac.kr>
2026-01-08 17:05:05 +0900

    feat: MenuRepository 클래스 기본 로직 추가

---
+++ src/main/java/menu/repository/MenuRepository.java
@@ -0,0 +1,44 @@
+package menu.repository;
+
+import menu.domain.menu.Menu;
+import menu.util.Parser;
+import menu.util.ResourceReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuRepository {
+    private static final int FIELD_SIZE = 2;
+    private static final String DEFAULT_PATH = "menus.md";
+
+    public MenuRepository() {
+    }
+
+    public List<Menu> findAll() {
+        List<Menu> menus = new ArrayList<>();
+        try (BufferedReader bufferedReader = ResourceReader.getBufferedReader(DEFAULT_PATH)) {
+            bufferedReader.readLine(); // header skip
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if(!line.trim().isBlank()) {
+                    menus.add(parseLine(line));
+                }
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("[ERROR] 파일 읽는 중 오류 발생: " + DEFAULT_PATH, e);
+        }
+        return menus;
+    }
+
+    private Menu parseLine(String line) {
+        List<String> parts = Parser.parseByDelimiter(line);
+
+        if (parts.size() != FIELD_SIZE) {
+            throw new IllegalStateException("[ERROR] 파일 형식 오류");
+        }
+
+        return new Menu(parts.get(0), parts.get(1));
+    }
+}
