d7617bac5380b3987906d507b56a7aea93fcf8b0
Junha Park <poiu7825@pusan.ac.kr>
2026-01-08 17:04:37 +0900

    feat: Coaches 클래스 추가

---
+++ src/main/java/menu/domain/Coach/Coaches.java
@@ -0,0 +1,11 @@
+package menu.domain.Coach;
+
+import java.util.List;
+
+public class Coaches {
+    private final List<Coach> coaches;
+
+    public Coaches(List<Coach> coaches) {
+        this.coaches = coaches;
+    }
+}
