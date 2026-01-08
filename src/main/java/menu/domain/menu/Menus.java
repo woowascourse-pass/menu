02e54fe3ea2817402ec3c638c78cf0ebd713c651
Junha Park <poiu7825@pusan.ac.kr>
2026-01-08 17:05:13 +0900

    feat: Menus 클래스 추가

---
+++ src/main/java/menu/domain/menu/Menus.java
@@ -0,0 +1,11 @@
+package menu.domain.menu;
+
+import java.util.List;
+
+public class Menus {
+    private final List<Menu> menus;
+
+    public Menus(List<Menu> menus) {
+        this.menus = menus;
+    }
+}
