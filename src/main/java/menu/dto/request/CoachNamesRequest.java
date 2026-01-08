a94b0d33e74782ba59cd9623982ec54acb11f7fb
Junha Park <poiu7825@pusan.ac.kr>
2026-01-08 18:29:20 +0900

    feat: CoachNamesRequest 클래스 유효성 검사 로직 추가

--- src/main/java/menu/dto/request/CoachNamesRequest.java
+++ src/main/java/menu/dto/request/CoachNamesRequest.java
@@ -2,5 +2,28 @@ package menu.dto.request;
 
 import java.util.List;
 
-public record CoachNamesRequest(List<String> coachNames) {
+import static menu.view.OutputView.PREFIX_ERROR;
+
+public record CoachNamesRequest(
+        List<String> coachNames
+) {
+    public CoachNamesRequest {
+        if (coachNames.size() < 2) {
+            throw new IllegalArgumentException(PREFIX_ERROR + "코치는 2명 이상 입력해야 합니다.");
+        }
+
+        if (coachNames.size() > 5) {
+            throw new IllegalArgumentException(PREFIX_ERROR + "코치는 5명 이하 입력해야 합니다.");
+        }
+
+        for (String coachName : coachNames) {
+            if (coachName.length() < 2) {
+                throw new IllegalArgumentException(PREFIX_ERROR + "코치 이름은 최소 2글자 입니다.");
+            }
+
+            if (coachName.length() > 4) {
+                throw new IllegalArgumentException(PREFIX_ERROR + "코치 이름은 최대 4글자 입니다.");
+            }
+        }
+    }
 }
