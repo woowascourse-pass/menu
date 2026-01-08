03b0fcaddc8b8385d2d85f7f6985223514938645
Junha Park <poiu7825@pusan.ac.kr>
2026-01-08 18:14:09 +0900

    feat: Coaches 클래스 기본 로직 추가

--- src/main/java/menu/domain/Coach/Coaches.java
+++ src/main/java/menu/domain/Coach/Coaches.java
@@ -2,10 +2,11 @@ package menu.domain.Coach;
 
 import java.util.List;
 
-public class Coaches {
-    private final List<Coach> coaches;
+public record Coaches(
+        List<Coach> coaches
+) {
 
-    public Coaches(List<Coach> coaches) {
-        this.coaches = coaches;
+    public int size() {
+        return coaches.size();
     }
 }
