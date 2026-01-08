ae73fa28a68e396a0cd2fd019da0323d78464dc8
Junha Park <poiu7825@pusan.ac.kr>
2026-01-08 17:02:39 +0900

    feat: Application 진입접 로직 추가

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,13 @@
 package menu;
 
+import menu.config.AppConfig;
+import menu.controller.MenuController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig appConfig = new AppConfig();
+
+        MenuController controller = appConfig.menuController();
+        controller.run();
     }
 }
