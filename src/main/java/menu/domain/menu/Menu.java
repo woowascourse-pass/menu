60b68f133de9a0dd528c37526733319f0d4c0c7b
Junha Park <poiu7825@pusan.ac.kr>
2026-01-08 17:04:50 +0900

    feat: Menu 클래스 추가

---
+++ src/main/java/menu/domain/menu/Menu.java
@@ -0,0 +1,11 @@
+package menu.domain.menu;
+
+public class Menu {
+    private final String name;
+    private final String category;
+
+    public Menu(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+}
